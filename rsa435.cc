ea7c48199d088a1d183c891bc4bf8c23cdcf1c60
THE YUNG T-SCAGS <theyungt-scags@ubuntu.ubuntu-domain>
2017-09-16 13:32:39 -0400

    Added prime generation

--- rsa435.cc
+++ rsa435.cc
@@ -3,11 +3,16 @@
 // Standard libraries
 #include <string>
 #include <iostream>
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <math.h>
 
 
 // `BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
+
+BigInteger genBigPrime();
+bool fTest(BigInteger);
+BigInteger modulo(BigInteger, BigInteger, BigInteger);
 
 int main() {
 	/* The library throws `const char *' error messages when things go
@@ -15,18 +20,14 @@ int main() {
 	 * one.  Your C++ compiler might need a command-line option to compile
 	 * code that uses exceptions. */
 	try {
-		      
+
       std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
       BigInteger big1 = BigInteger(1);
-      for (int i=0;i<400;i++) {
-         big1 = big1*10 +rand();
-      }
-      std::cout << "my big1 !!!\n";
+			big1 = genBigPrime();
+			std::cout << "my big1 !!!\n";
       std::cout << big1;
       BigInteger big2 = BigInteger(1);
-      for (int i=0;i<400;i++) {
-         big2 = big2*10 +rand();
-      }
+      big2 = genBigPrime();
       std::cout << "my big2 !!!\n";
       std::cout << big2;
       std::cout << "my big3 = big1*big2 !!!\n";
@@ -34,11 +35,47 @@ int main() {
       std::cout <<big3;
       std::cout << "my big3/big2 !!!\n";
       std::cout <<big3/big2;
-      
+
 	} catch(char const* err) {
 		std::cout << "The library threw an exception:\n"
 			<< err << std::endl;
 	}
 
 	return 0;
+}
+
+BigInteger genBigPrime(){
+	BigInteger big1 = BigInteger(1);
+	do{
+		big1 = 1;
+		for (int i=0;i<400;i++) {
+			 big1 = big1*10 +rand();
+		}
+	}while(!fTest(big1));
+	return big1;
+}
+
+bool fTest(BigInteger p){
+	if (p == 1){
+		return false;
+	}
+	for (int i = 2; i < 6; i++){
+			if (modulo(i, p - 1, p) != 1){
+					return false;
+			}
+		}
+		return true;
+}
+
+BigInteger modulo(BigInteger base, BigInteger exponent, BigInteger mod){
+    BigInteger x = 1;
+    BigInteger y = base;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+            x = (x * y) % mod;
+        y = (y * y) % mod;
+        exponent = exponent / 2;
+    }
+    return x % mod;
 }
