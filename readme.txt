489784cde8b0543af14c1941d0f280c4a7aeb5c6
THE YUNG T-SCAGS <theyungt-scags@ubuntu.ubuntu-domain>
2017-09-27 22:35:29 -0400

    finished project requirements

---
+++ readme.txt
@@ -0,0 +1,14 @@
+Anthony Scaglione, University of Akron
+ams390@zips.uakron.edu
+september 27th, 2017
+
+This program can generate encryption keys for RSA435, and use them to
+sign and verify documents
+
+to compile, add all other files listed here to the project directory that
+contains the provided project files, replacing the existing rsa435.cc and the
+provided makefile as well.
+
+Program can be run with s and v arguments as well as the filename
+Example: ./rsa435 s file.txt
+Additionally, adding a k argument instead will generate keys.
--- rsa435.cc
+++ rsa435.cc
@@ -1,6 +1,9 @@
 //Anthony Scaglione, University of Akron
-
-// You need to complete this program for a part of your first project.
+//ams390@zips.uakron.edu
+//september 27th, 2017
+//
+//This program can generate encryption keys for RSA435, and use them to
+//sign and verify documents
 
 // Standard libraries
 #include <string>
@@ -17,6 +20,7 @@
 // `BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
 
+//functons implemented
 BigInteger genBigPrime();
 bool fTest(BigInteger);
 BigInteger modulo(BigInteger, BigInteger, BigInteger);
@@ -24,24 +28,29 @@ BigInteger exEuc(BigInteger ,BigInteger);
 void keyGen(BigInteger, BigInteger);
 void sign(std::string);
 void verify(std::string);
+BigInteger modInv(BigInteger, BigInteger, BigInteger &, BigInteger &);
 
-int main() {
+
+
+int main(int argc, char** argv) {
 	/* The library throws `const char *' error messages when things go
 	 * wrong.  It's a good idea to catch them using a `try' block like this
 	 * one.  Your C++ compiler might need a command-line option to compile
 	 * code that uses exceptions. */
 	try {
+		std::string arg = argv[1];
+		if(arg == "k"){
+			std::cout << "Generating Keys..." << std::endl;
 			BigInteger p = BigInteger(1);
 			BigInteger q = BigInteger(1);
-			//keyGen(p,q);
-			sign("file.txt");
-			verify("file.txt.signed");
-
-			//BigInteger outputInt = stringToBigInteger(output);
-
-			//std::cout << sign("file.txt") << std::endl;
-
-
+			keyGen(p,q);
+		}else if(arg == "s"){
+			sign(argv[2]);
+		}else if(arg == "v"){
+			verify(argv[2]);
+		}else{
+			std::cout << "Incorrect arguments: Use k for keygen, s to sign, v to verify" << std::endl;
+		}
 	} catch(char const* err) {
 		std::cout << "The library threw an exception:\n"
 			<< err << std::endl;
@@ -49,6 +58,10 @@ int main() {
 
 	return 0;
 }
+
+//verifies the file based on m^e%n=c
+//verification occurs when the encryption of the saved hash code matches
+//the encoded content part of the file
 void verify(std::string fileName){
 	//fetch decrypted signature from file
 	std::ifstream ifs(fileName.c_str());
@@ -61,43 +74,48 @@ void verify(std::string fileName){
 	std::string::size_type i = input.find(decrypted);
 	if (i != std::string::npos)
   	input.erase(i, decrypted.length());
+	//if file isnt a txt file, place saved hash in the right place
+	if (!(fileName.find(".txt") != std::string::npos)) {
+	  input.resize (input.size () - 1);
+	}
 	//generate hash of content
 	std::string shaOut = sha256(input);
 	//fetch keys from keyfile
 	BigUnsigned content = BigUnsignedInABase(shaOut, 16);
-	std::cout << content << std::endl;
+	ifs.close();
 	ifs.open("e_n.txt");
 	std::string eIn = "";
-	getline(ifs,eIn); //get e
+	std::getline(ifs,eIn); //get e
 	BigInteger e = stringToBigInteger(eIn);
 	std::string nIn = "";
-	getline(ifs,nIn); //get n
+	std::getline(ifs,nIn); //get n
 	BigInteger n = stringToBigInteger(nIn);
-
-	std::cout << modulo(stringToBigInteger(decrypted), e, n) << std::endl;
-
 	BigInteger m = stringToBigInteger(decrypted);
 
 	//test verification
 	if(modulo(m, e, n) == content){
-		std::cout << "verified" << std::endl;
+		std::cout << "The file has been verified" << std::endl;
 	}else{
-		std::cout << "fail" << std::endl;
+		std::cout << "The file has failed verification" << std::endl;
 	}
 
 
 }
 
 //signs a file, given the filename
+//saves the resulting content and hash code as a new file
+//listed as [filename].signed
 void sign(std::string fileName){
 	//fetch data from text files
-	std::ifstream ifs(fileName.c_str());
-	std::string input((std::istreambuf_iterator<char>(ifs)),(std::istreambuf_iterator<char>()));
+	std::ifstream ifs(fileName.c_str(), std::ios::in | std::ios::binary);
+	std::ostringstream oss;
+	oss << ifs.rdbuf();
+	std::string data(oss.str());
+	//std::string input((std::istreambuf_iterator<char>(ifs)),(std::istreambuf_iterator<char>()));
 	ifs.close();
-	std::string shaOut = sha256(input);
+	std::string shaOut = sha256(data);
 	//fetch keys from keyfile
 	BigUnsigned c = BigUnsignedInABase(shaOut, 16);
-	std::cout << c << std::endl;
 	ifs.open("d_n.txt");
 	std::string dIn = "";
 	getline(ifs,dIn); //get d
@@ -108,18 +126,27 @@ void sign(std::string fileName){
 	//return signed value
 	fileName = fileName + ".signed";
 	std::ofstream ofs(fileName.c_str(), std::ios::out | std::ios::trunc);
-	ofs << input;
+	ofs << data;
+	if (!(fileName.find(".txt") != std::string::npos)) {
+    ofs << std::endl;
+	}
 	ofs << modulo(c, d, n);
 	ofs.close();
 }
 
+//Function generates the keys required for encryption.
+//e is constant, while d is computed
+//both saved in e_n.txt and d_n.txt respectively
 void keyGen(BigInteger p, BigInteger q){
 	p = genBigPrime();
 	q = genBigPrime();
 	BigInteger n = p*q;
 	BigInteger phi = (p-1)*(q-1);
 	BigInteger e = 65537;
-	BigInteger d = exEuc(phi, e);
+	BigInteger x, y;
+	BigInteger d = modInv(phi, e, x, y);
+	if(d < 0)
+		d += phi;
 	BigInteger test = (e*d)%phi;
 	//write the found keys to a file
 	std::ofstream myfile;
@@ -129,32 +156,6 @@ void keyGen(BigInteger p, BigInteger q){
 	myfile.open("d_n.txt");
 	myfile << d << "\n" << n;
 	myfile.close();
-}
-
-//extended euclidean algorithm
-//really sketchy but functional implimentation of extended euclidean
-//as explained in this video https://www.youtube.com/watch?v=Z8M2BTscoD4&t=704s
-//
-//returns the value of d
-BigInteger exEuc(BigInteger a, BigInteger b){
-	BigInteger x = 0;
-	BigInteger prevX = 1; //holds the previous value of x
-	BigInteger y = 1;
-	BigInteger prevY = 0; //holds the previous value of y
-	BigInteger temp, q;
-		while(b != 0){
-			q = a / b;
-			temp = b;
-			b = a % b;
-			a = temp;
-			temp = x;
-			x = prevX-q*x;
-			prevX = temp;
-			temp = y;
-			y = prevY-q*y;
-			prevY = temp;
-	  }
-	  return prevY; //this ends up being d
 }
 
 //generates large prime numbers
@@ -180,7 +181,7 @@ bool fTest(BigInteger p){
 		return true;
 }
 
-
+//calculates and returns x^y%m
 BigInteger modulo(BigInteger x, BigInteger y, BigInteger m){
 	if(y == 0){
 		return 1;
@@ -191,4 +192,19 @@ BigInteger modulo(BigInteger x, BigInteger y, BigInteger m){
 	}else{
 		return (z*z*x)%m;
 	}
+}
+
+//extended euclidean
+//finds modular inverse
+//stackoverflow is a lifesaver
+BigInteger modInv(BigInteger a, BigInteger b, BigInteger &x, BigInteger &y) {
+    if(b == 0) {
+       x = 1;
+       y = 0;
+       return a;
+    }
+    BigInteger x1, y1, gcd = modInv(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return y; //y is what returns as the value of d
 }
